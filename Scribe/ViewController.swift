23f5c6f4fe76233d1c427b0665462e9175893d41
Sattar Hummatli <settarxan@gmail.com>
2017-07-22 08:23:06 +0400

    created project

---
+++ Scribe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scribe
+//
+//  Created by Settar Hummetli on 7/22/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
